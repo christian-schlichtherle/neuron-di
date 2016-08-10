ccb1c006d593ffe81218ef0ca49a831afb32469f
Christian Schlichtherle <christian@schlichtherle.de>
2016-08-10 17:18:23 +0200

    Improves specification of caching strategies.

--- src/main/java/org/neuron_di/api/CachingStrategy.java
+++ src/main/java/org/neuron_di/api/CachingStrategy.java
@@ -13,7 +13,8 @@ public enum CachingStrategy {
 
     /**
      * Does not cache the return value of the annotated method:
-     * A subsequent call by any thread may return another instance.
+     * Although not strictly required, a subsequent call by any thread should
+     * return another instance.
      */
     DISABLED {
 
@@ -28,7 +29,7 @@ public enum CachingStrategy {
      * Although not strictly required, a subsequent call by the same thread
      * should return the same instance.
      * A subsequent call by any other thread may return another instance.
-     * This definition recursively applies to all threads.
+     * This definition recursively applies to any thread.
      */
     NOT_THREAD_SAFE {
 
@@ -55,7 +56,7 @@ public enum CachingStrategy {
      * A subsequent call by the same thread must return the same instance.
      * Although not strictly required, a subsequent call by any other thread
      * should return another instance.
-     * This definition recursively applies to all threads.
+     * This definition recursively applies to any thread.
      */
     THREAD_LOCAL {
 
