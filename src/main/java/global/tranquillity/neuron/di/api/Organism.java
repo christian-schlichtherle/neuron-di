dd376d788edbf1657078bfe0319d52c689317b93
Christian Schlichtherle <christian@schlichtherle.de>
2016-08-13 19:56:47 +0200

    Removes unused method.

--- src/main/java/global/tranquillity/neuron/di/api/Organism.java
+++ src/main/java/global/tranquillity/neuron/di/api/Organism.java
@@ -127,15 +127,4 @@ public class Organism {
         e.setCallbacks(helper.getCallbacks());
         return e.create();
     }
-
-    /**
-     * Returns an instance of the given type which will resolve it's
-     * non-constant dependencies to mock objects for testing.
-     * The instance will ignore any caching strategy configured for its
-     * {@linkplain Caching synapses} and use {@link CachingStrategy#THREAD_SAFE}
-     * in order to enable stubbing and verifying the mocked dependencies.
-     */
-    public <T> T test(Class<T> type) {
-        throw new UnsupportedOperationException();
-    }
 }
