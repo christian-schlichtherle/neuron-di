2670a32e1ba5d0b205d6473bb744fbaf5ca700c9
Christian Schlichtherle <christian@schlichtherle.de>
2016-09-02 12:44:30 +0200

    Improves test coverage of binding DSL.

--- guice/src/test/scala/global/namespace/neuron/di/guice/BinderLikeSpec.scala
+++ guice/src/test/scala/global/namespace/neuron/di/guice/BinderLikeSpec.scala
@@ -13,14 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package global.namespace.neuron.di.guice.scala.test
+package global.namespace.neuron.di.guice
 
 import com.google.inject.binder.{AnnotatedBindingBuilder, AnnotatedConstantBindingBuilder, ConstantBindingBuilder, ScopedBindingBuilder}
 import com.google.inject.name.Names.named
-import com.google.inject.{Binder, Injector, Key, Provider => gProvider}
+import com.google.inject.{Binder, Injector, Provider}
 import global.namespace.neuron.di.api.scala.Incubator
 import global.namespace.neuron.di.guice.scala._
-import global.namespace.neuron.di.guice.{BinderLike => jBinderLike}
 import org.mockito.ArgumentCaptor
 import org.mockito.Mockito._
 import org.scalatest.Matchers._
@@ -33,43 +32,39 @@ class BinderLikeSpec extends WordSpec with Mockito {
 
     val binder = mock[Binder]
 
-    val binderLike = Incubator
-      .stub[jBinderLike]
-      .bind(_.binder).to(binder)
-      .breed
-
-    "bind named constants" in {
+    "bind a named constant" in {
       val builder1 = mock[AnnotatedConstantBindingBuilder]
       val builder2 = mock[ConstantBindingBuilder]
 
       binder.bindConstant returns builder1
       builder1.annotatedWith(named("foo")) returns builder2
 
-      binderLike.bindConstantNamed("foo") should be theSameInstanceAs builder2
+      binder.bindConstantNamed("foo") should be theSameInstanceAs builder2
     }
 
-    "bind neurons" in {
-      val builder1 = mock[AnnotatedBindingBuilder[jBinderLike]]
+    "bind a neuron" in {
+      val builder1 = mock[AnnotatedBindingBuilder[BinderLike]]
       val builder2 = mock[ScopedBindingBuilder]
-      val injectorProvider = mock[gProvider[Injector]]
+      val injectorProvider = mock[Provider[Injector]]
       val injector = mock[Injector]
-      val binderProvider = mock[gProvider[Binder]]
 
-      binder.bindClass[jBinderLike] returns builder1
-      builder1.toProvider(any[gProvider[jBinderLike]]) returns builder2
+      binder.bindClass[BinderLike] returns builder1
+      builder1.toProvider(any[Provider[BinderLike]]) returns builder2
       binder.getProvider(classOf[Injector]) returns injectorProvider
       injectorProvider.get returns injector
-      injector.getProvider(any[Key[Binder]]) returns binderProvider
 
-      binderLike.bindNeuron(classOf[jBinderLike]) should be theSameInstanceAs builder2
+      binder.bindNeuronClass[BinderLike] should be theSameInstanceAs builder2
 
       there was one(binder).getProvider(classOf[Injector])
 
-      val binderLikeProvider = ArgumentCaptor.forClass(classOf[gProvider[jBinderLike]])
-      verify(builder1).toProvider(binderLikeProvider.capture)
-      binderLikeProvider.getValue.get shouldBe a[jBinderLike]
+      val neuronProviderCaptor = ArgumentCaptor.forClass(classOf[Provider[BinderLike]])
+      verify(builder1).toProvider(neuronProviderCaptor.capture)
+      val neuronProvider = neuronProviderCaptor.getValue.asInstanceOf[NeuronProvider[BinderLike]]
+      neuronProvider.injector should be theSameInstanceAs injector
+      neuronProvider.runtimeClass should be theSameInstanceAs classOf[BinderLike]
 
       there was one(injectorProvider).get
+      there were noMoreCallsTo(injector)
     }
   }
 }
