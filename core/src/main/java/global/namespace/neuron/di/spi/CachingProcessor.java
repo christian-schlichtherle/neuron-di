7f15ad589490ebf9d35874bdb7264c2bc8cbf082
Christian Schlichtherle <christian@schlichtherle.de>
2016-09-05 22:47:28 +0200

    Adds processors for @Neuron and @Caching annotations.

---
+++ core/src/main/java/global/namespace/neuron/di/spi/CachingProcessor.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2016 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package global.namespace.neuron.di.spi;
+
+import global.namespace.neuron.di.api.Caching;
+import global.namespace.neuron.di.api.Neuron;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+import static javax.lang.model.element.Modifier.*;
+import static javax.tools.Diagnostic.Kind.ERROR;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes("global.namespace.neuron.di.api.Caching")
+@SupportedOptions("global.namespace.neuron.di.spi.verbose")
+public final class CachingProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
+        roundEnv.getElementsAnnotatedWith(Caching.class)
+                .forEach(elem -> validateMethod((ExecutableElement) elem));
+        return true;
+    }
+
+    private void validateMethod(final ExecutableElement method) {
+        final Set<Modifier> modifiers = method.getModifiers();
+        if (modifiers.contains(FINAL)) {
+            error("A caching method must not be final.", method);
+        }
+        if (modifiers.contains(PRIVATE)) {
+            error("A caching method must not be private.", method);
+        }
+        if (modifiers.contains(STATIC)) {
+            error("A caching method must not be static.", method);
+        }
+        if (!method.getParameters().isEmpty()) {
+            error("A caching method must not have parameters.", method);
+        }
+        if (null == method.getEnclosingElement().getAnnotation(Neuron.class)) {
+            error("A caching method must be a member of a neuron class or interface.", method);
+        }
+    }
+
+    private void error(CharSequence message, Element e) {
+        getMessager().printMessage(ERROR, message , e);
+    }
+
+    private Messager getMessager() { return processingEnv.getMessager(); }
+}
---
+++ core/src/main/java/global/namespace/neuron/di/spi/NeuronProcessor.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2016 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package global.namespace.neuron.di.spi;
+
+import global.namespace.neuron.di.api.Neuron;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+import static javax.lang.model.element.ElementKind.CONSTRUCTOR;
+import static javax.lang.model.element.Modifier.*;
+import static javax.tools.Diagnostic.Kind.ERROR;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes("global.namespace.neuron.di.api.Neuron")
+public final class NeuronProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
+        roundEnv.getElementsAnnotatedWith(Neuron.class)
+                .stream()
+                .filter(elem -> elem.getKind().isClass())
+                .forEach(elem -> validateClass((TypeElement) elem));
+        return true;
+    }
+
+    private void validateClass(final TypeElement type) {
+        final Set<Modifier> modifiers = type.getModifiers();
+        if (modifiers.contains(FINAL)) {
+            error("A neuron class must not be final.", type);
+        }
+        if (type.getNestingKind().isNested()) {
+            if (!modifiers.contains(STATIC)) {
+                error("A neuron class must be static.", type);
+            }
+        }
+        if (!hasNonPrivateConstructorWithoutParameters(type)) {
+            error("A neuron class must have a non-private constructor without parameters.", type);
+        }
+    }
+
+    private boolean hasNonPrivateConstructorWithoutParameters(TypeElement type) {
+        return type
+                .getEnclosedElements()
+                .stream()
+                .filter(elem -> elem.getKind() == CONSTRUCTOR)
+                .anyMatch(elem -> isNonPrivateConstructorWithoutParameters((ExecutableElement) elem));
+    }
+
+    private boolean isNonPrivateConstructorWithoutParameters(ExecutableElement ctor) {
+        return !ctor.getModifiers().contains(PRIVATE) &&
+                ctor.getParameters().isEmpty();
+    }
+
+    private void error(CharSequence message, Element e) {
+        getMessager().printMessage(ERROR, message , e);
+    }
+
+    private Messager getMessager() { return processingEnv.getMessager(); }
+}
