bdbc9944a67f8ef5622c0f344c9435aa096d2c0b
Christian Schlichtherle <christian@schlichtherle.de>
2016-12-26 12:03:07 +0100

    Revert "Adds more statements to the binding DSL. This change breaks backwards compatibility of the byte code (but not of the source code), so users need to recompile their source code."
    This reverts commit e07c4f1815c76e30093075a12b066ff083fe1f18.

--- core-scala/src/main/scala/global/namespace/neuron/di/scala/Incubator.scala
+++ core-scala/src/main/scala/global/namespace/neuron/di/scala/Incubator.scala
@@ -16,10 +16,10 @@
 package global.namespace.neuron.di.scala
 
 import java.lang.reflect.Method
-import java.util.function.{Function => jFunction}
+import java.util.function.{Function => jFunction, Supplier => jSupplier}
 
+import global.namespace.neuron.di.java.{DependencyResolver, DependencyProvider, Incubator => jIncubator}
 import global.namespace.neuron.di.internal.scala.runtimeClassOf
-import global.namespace.neuron.di.java.{DependencyProvider, DependencyResolver, Incubator => jIncubator}
 
 import scala.reflect._
 
@@ -40,32 +40,17 @@ object Incubator {
       self
     }
 
-    def bind[B](method: A => B, to: => B): self.type = {
-      jstub bind method to to _
-      self
-    }
+    case class bind[B](methodReference: A => B) {
 
-    case class bind[B](method: A => B) {
-
-      private val jbind = jstub bind method
+      private val jbind = jstub bind methodReference
 
       def to(value: => B): self.type = {
         jstub = jbind to value _
         self
       }
 
-      def apply(to: => B): self.type = {
-        jstub = jbind to to _
-        self
-      }
-
       def to[C <: B](function: A => C): self.type = {
         jstub = jbind to function
-        self
-      }
-
-      def apply[C <: B](to: A => C): self.type = {
-        jstub = jbind to to
         self
       }
     }
