c6ac39b3af96784b16e381fd11115d7d76066569
Christian Schlichtherle <christian@schlichtherle.de>
2017-01-01 14:16:40 +0100

    Improves diagnostic message.

--- core-scala/src/main/scala/global/namespace/neuron/di/scala/Caching.scala
+++ core-scala/src/main/scala/global/namespace/neuron/di/scala/Caching.scala
@@ -19,7 +19,7 @@ import scala.annotation.{StaticAnnotation, compileTimeOnly}
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-@compileTimeOnly("You need to apply the Macro Paradise plugin to the Scala compiler to use this macro annotation. See http://docs.scala-lang.org/overviews/macros/paradise .")
+@compileTimeOnly("You need to add the Macro Paradise plugin to the Scala compiler to use this macro annotation. See http://docs.scala-lang.org/overviews/macros/paradise .")
 class Caching(value: CachingStrategy = CachingStrategy.THREAD_SAFE)
   extends StaticAnnotation {
 
