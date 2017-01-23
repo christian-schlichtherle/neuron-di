0461a2302f23005e8c2ffb821eb9c0dc577482de
Christian Schlichtherle <christian@schlichtherle.de>
2017-01-23 18:37:29 +0100

    When autowiring, fields and static members can be used, too.

--- core/src/main/java/global/namespace/neuron/di/java/Reflection.java
+++ core/src/main/java/global/namespace/neuron/di/java/Reflection.java
@@ -1,8 +1,11 @@
 package global.namespace.neuron.di.java;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.reflect.Method;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static java.lang.invoke.MethodHandles.publicLookup;
 import static java.lang.invoke.MethodType.methodType;
@@ -12,40 +15,56 @@ class Reflection {
     private Reflection() { }
 
     static Find find(final String member) {
-        return new Find() {
+        return o -> new Function<Class<?>, Optional<MethodHandle>>() {
             @Override
-            public Optional<MethodHandle> in(final Class<?> c) {
+            public Optional<MethodHandle> apply(final Class<?> c) {
                 try {
-                    final Method method = c.getDeclaredMethod(member);
-                    method.setAccessible(true);
-                    return Optional.of(publicLookup()
-                            .unreflect(method)
-                            .asType(methodType(Object.class, c)));
+                    return methodHandle(c.getDeclaredMethod(member), publicLookup()::unreflect);
                 } catch (final NoSuchMethodException ignored) {
-                    Optional<MethodHandle> result;
-                    for (final Class<?> iface : c.getInterfaces()) {
-                        if ((result = in(iface)).isPresent()) {
-                            return result;
+                    try {
+                        return methodHandle(c.getDeclaredField(member), publicLookup()::unreflectGetter);
+                    } catch (final NoSuchFieldException ignoredAgain) {
+                        Optional<MethodHandle> result;
+                        for (final Class<?> iface : c.getInterfaces()) {
+                            if ((result = apply(iface)).isPresent()) {
+                                return result;
+                            }
                         }
-                    }
-                    final Class<?> zuper = c.getSuperclass();
-                    if (null != zuper) {
-                        if ((result = in(zuper)).isPresent()) {
-                            return result;
+                        final Class<?> zuper = c.getSuperclass();
+                        if (null != zuper) {
+                            if ((result = apply(zuper)).isPresent()) {
+                                return result;
+                            }
                         }
+                        return Optional.empty();
                     }
-                    return Optional.empty();
+                }
+            }
+
+            <M extends AccessibleObject & Member>
+            Optional<MethodHandle> methodHandle(M member, Unreflect<M> unreflect) {
+                member.setAccessible(true);
+                MethodHandle mh;
+                try {
+                    mh = unreflect.apply(member);
                 } catch (IllegalAccessException e) {
                     throw new AssertionError(e);
                 }
+                if (0 == (member.getModifiers() & Modifier.STATIC)) {
+                    mh = mh.bindTo(o);
+                }
+                return Optional.of(mh.asType(methodType(Object.class)));
             }
-        };
+        }.apply(o.getClass());
+    }
+
+    private interface Unreflect<M> {
+
+        MethodHandle apply(M member) throws IllegalAccessException;
     }
 
     interface Find {
 
-        default Optional<MethodHandle> in(Object o) { return in(o.getClass()).map(mh -> mh.bindTo(o)); }
-
-        Optional<MethodHandle> in(Class<?> c);
+        Optional<MethodHandle> in(Object o);
     }
 }
