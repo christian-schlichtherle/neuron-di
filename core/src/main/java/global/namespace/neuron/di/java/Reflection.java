30952a4466afb006e86fe86bc24160be8191e730
Christian Schlichtherle <christian@schlichtherle.de>
2017-01-23 23:34:25 +0100

    Adds tests and improves performance.

--- core/src/main/java/global/namespace/neuron/di/java/Reflection.java
+++ core/src/main/java/global/namespace/neuron/di/java/Reflection.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright © 2016 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package global.namespace.neuron.di.java;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 import static java.lang.invoke.MethodHandles.publicLookup;
@@ -12,50 +30,63 @@ import static java.lang.invoke.MethodType.methodType;
 
 class Reflection {
 
+    private static final MethodType objectMethodType = methodType(Object.class);
+
     private Reflection() { }
 
     static Find find(final String member) {
-        return o -> new Function<Class<?>, Optional<MethodHandle>>() {
-            @Override
-            public Optional<MethodHandle> apply(final Class<?> c) {
-                try {
-                    return methodHandle(c.getDeclaredMethod(member), publicLookup()::unreflect);
-                } catch (final NoSuchMethodException ignored) {
-                    try {
-                        return methodHandle(c.getDeclaredField(member), publicLookup()::unreflectGetter);
-                    } catch (final NoSuchFieldException ignoredAgain) {
-                        Optional<MethodHandle> result;
-                        for (final Class<?> iface : c.getInterfaces()) {
-                            if ((result = apply(iface)).isPresent()) {
-                                return result;
-                            }
-                        }
-                        final Class<?> zuper = c.getSuperclass();
-                        if (null != zuper) {
-                            if ((result = apply(zuper)).isPresent()) {
-                                return result;
-                            }
-                        }
-                        return Optional.empty();
-                    }
-                }
-            }
+        return new Find() {
 
-            <M extends AccessibleObject & Member>
-            Optional<MethodHandle> methodHandle(M member, Unreflect<M> unreflect) {
-                member.setAccessible(true);
-                MethodHandle mh;
-                try {
-                    mh = unreflect.apply(member);
-                } catch (IllegalAccessException e) {
-                    throw new AssertionError(e);
-                }
-                if (0 == (member.getModifiers() & Modifier.STATIC)) {
-                    mh = mh.bindTo(o);
-                }
-                return Optional.of(mh.asType(methodType(Object.class)));
+            final Set<Class<?>> interfaces = new HashSet<>();
+
+            @Override
+            public Optional<MethodHandle> in(Object o) {
+                return new Function<Class<?>, Optional<MethodHandle>>() {
+                    @Override
+                    public Optional<MethodHandle> apply(final Class<?> c) {
+                        try {
+                            return methodHandle(c.getDeclaredMethod(member), publicLookup()::unreflect);
+                        } catch (final NoSuchMethodException ignored) {
+                            try {
+                                return methodHandle(c.getDeclaredField(member), publicLookup()::unreflectGetter);
+                            } catch (final NoSuchFieldException ignoredAgain) {
+                                Optional<MethodHandle> result;
+                                for (final Class<?> iface : c.getInterfaces()) {
+                                    if (!interfaces.contains(iface)) {
+                                        if ((result = apply(iface)).isPresent()) {
+                                            return result;
+                                        }
+                                        interfaces.add(iface);
+                                    }
+                                }
+                                final Class<?> zuper = c.getSuperclass();
+                                if (null != zuper) {
+                                    if ((result = apply(zuper)).isPresent()) {
+                                        return result;
+                                    }
+                                }
+                                return Optional.empty();
+                            }
+                        }
+                    }
+
+                    <M extends AccessibleObject & Member>
+                    Optional<MethodHandle> methodHandle(M member, Unreflect<M> unreflect) {
+                        member.setAccessible(true);
+                        MethodHandle mh;
+                        try {
+                            mh = unreflect.apply(member);
+                        } catch (IllegalAccessException e) {
+                            throw new AssertionError(e);
+                        }
+                        if (0 == (member.getModifiers() & Modifier.STATIC)) {
+                            mh = mh.bindTo(o);
+                        }
+                        return Optional.of(mh.asType(objectMethodType));
+                    }
+                }.apply(o.getClass());
             }
-        }.apply(o.getClass());
+        };
     }
 
     private interface Unreflect<M> {
---
+++ core/src/test/scala/global/namespace/neuron/di/java/ReflectionSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2016 Schlichtherle IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package global.namespace.neuron.di.java
+
+import java.lang.invoke.MethodHandle
+import java.util.Optional
+
+import global.namespace.neuron.di.java.Reflection._
+import global.namespace.neuron.di.java.sample.{A, HasPrivateMembers}
+import org.scalatest.Matchers._
+import org.scalatest.WordSpec
+
+class ReflectionSpec extends WordSpec {
+
+  implicit class WithOptionalMethodHandle(omh: Optional[MethodHandle]) {
+
+    def as[A]: A = omh.get.invokeExact().asInstanceOf[A]
+  }
+
+  "Reflection.find(...).in(...)" when {
+    "looking up members in a subclass of `A`" should {
+      val a = new A { }
+
+      "find `a`" in {
+        find("a").in(a).as[A] should be theSameInstanceAs a
+      }
+    }
+
+    "looking up members in a subclass of `HasPrivateMembers`" should {
+      val hasPrivateMembers = new HasPrivateMembers { }
+
+      "find `method`" in {
+        find("method").in(hasPrivateMembers).as[Int] shouldBe 1
+      }
+
+      "find `staticMethod`" in {
+        find("staticMethod").in(hasPrivateMembers).as[Long] shouldBe 2L
+      }
+
+      "find `field`" in {
+        find("field").in(hasPrivateMembers).as[Boolean] shouldBe true
+      }
+
+      "find `staticField`" in {
+        find("staticField").in(hasPrivateMembers).as[Char] shouldBe '?'
+      }
+    }
+  }
+}
